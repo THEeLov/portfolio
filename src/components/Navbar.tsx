f359fb661d02a57aab05bacabb5f40f97c2a543c
Filip Kozik <536410@mail.muni.cz>
2024-03-23 13:34:42 +0100

    porfolio finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,17 +33,6 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
-      <div className="navbar__icon-container">
-        <img className="navbar__icon" src={MenuIcon} alt="Menu icon" />
-      </div>
-      <nav className="navbar">
-        <ul className="navbar__list">
-          <li className="navbar__list__item">About</li>
-          <li className="navbar__list__item">Courses</li>
-          <li className="navbar__list__item">Contact</li>
-        </ul>
-      </nav>
     </div>
   );
 };
